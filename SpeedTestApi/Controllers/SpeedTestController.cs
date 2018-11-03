79be5a21a320781142a559525548c0da87fe9b29
Malgorzata Kokoszka <mal@computas.com>
2018-11-03 16:24:24 +0100

    new stuff

--- SpeedTestApi/Controllers/SpeedTestController.cs
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using SpeedTestApi.Models;
     
 namespace SpeedTestApi.Controllers
 {
@@ -18,6 +18,16 @@ namespace SpeedTestApi.Controllers
         {
             return Ok("PONG");
         }
+
+    // POST speedtest/
+    [HttpPost]
+    public ActionResult<string> UploadSpeedTest([FromBody] TestResult speedTest)
+    {
+        var speedTestData = $"Got a TestResult from { speedTest.User } with download { speedTest.Data.Speeds.Download } Mbps.";
+
+        return Ok(speedTestData);
+    }
+
     }
 }
 
